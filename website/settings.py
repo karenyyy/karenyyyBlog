a18d08a7484051a050376b15e097a4fee6da8cb1
karenyyy <karenye.psu@gmail.com>
2017-12-01 03:29:28 -0500

    change iddr

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0',
                  '127.0.0.1',
-                 'kyyysimpleblog.herokuapp.com']
+                 'karenyyyme.herokuapp.com']
 
 # Application definition
 
