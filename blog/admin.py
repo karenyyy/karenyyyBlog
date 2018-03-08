e69c9cb103c8245ae759275901df3c6cb4d14aaa
karenyyy <karenye.psu@gmail.com>
2018-03-07 19:49:22 -0500

    update

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,8 @@ from .models import Post, Category, Tag
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+    list_filter = ('created_time', 'category', 'author')
+    search_fields = ('title', 'body')
 
 
 admin.site.register(Post, PostAdmin)
